5223f3c08de2761878f966f7fd93c4010e718c0f
Your Name <your-email@example.com>
2025-12-17 20:05:19 +0500

    changes by nayab

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+const sections = document.querySelectorAll(".section");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".card");
-  cards.forEach(card => {
-    card.style.opacity = 0;
-    card.style.transform = "translateY(40px)";
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+      entry.target.style.transition = "all 0.8s ease";
+    }
   });
+});
 
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.style.opacity = 1;
-        entry.target.style.transform = "translateY(0)";
-        entry.target.style.transition = "all 0.6s ease";
-      }
-    });
-  });
-
-  cards.forEach(card => observer.observe(card));
+sections.forEach(section => {
+  section.style.opacity = 0;
+  section.style.transform = "translateY(40px)";
+  observer.observe(section);
 });
